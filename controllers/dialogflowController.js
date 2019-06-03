2e044823c8512f215ebbc2aeacf98eb780860607
Arash Paknezad <a.paknezad@hotmail.com>
2019-06-03 18:33:09 +0200

    added comments to code

--- controllers/dialogflowController.js
+++ controllers/dialogflowController.js
@@ -5,7 +5,10 @@ const dialogflowService = require('../services/dialogflowService');
 module.exports.post = (req, res) => {
     const agent = new WebhookClient({request: req, response: res});
     const intentMap = new Map();
+
+    // dialogflowService.question method handles the question intent
     intentMap.set('Oefening', dialogflowService.question);
+    // dialogflowService.answer method handles the answer intent
     intentMap.set('Oefening - antwoord', dialogflowService.answer);
     agent.handleRequest(intentMap)
 };
