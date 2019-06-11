0256ac9250c07617af66b61de8de3afd84eac6e2
Guus <guus.dijkhuis@gmail.com>
2019-06-11 15:52:14 +0200

    slopen

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ autoroute(app, {
 );
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port, () => console.log(`Server running on port ${port}`))
