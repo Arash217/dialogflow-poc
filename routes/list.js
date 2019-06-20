e1d34107f28715a7463d961df27045a0e7953809
Arash Paknezad <a.paknezad@hotmail.com>
2019-06-20 14:48:04 +0200

    fix conflict

--- routes/list.js
+++ routes/list.js
@@ -2,24 +2,17 @@ const express = require("express");
 const listController = require("../controllers/listController");
 
 module.exports.autoroute = {
-<<<<<<< HEAD
-  get: {
-    "/lijsten": listController.get,
-    "/lijsten/toevoegen": listController.add
-  },
-=======
     get: {
         '/lijsten': listController.get,
         '/lijsten/toevoegen': listController.add,
         '/lijsten/:id': listController.update
     },
->>>>>>> dev
 
-  post: {
-    "/lijsten": [express.json(), listController.create]
-  },
+    post: {
+        "/lijsten": [express.json(), listController.create]
+    },
 
-  delete: {
-    "/lijsten": [express.json(), listController.remove]
-  }
+    delete: {
+        "/lijsten": [express.json(), listController.remove]
+    }
 };
