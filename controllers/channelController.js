069dee456c1dfeaded3da4f9cf91ed6ed1e0052c
Arash Paknezad <a.paknezad@hotmail.com>
2019-06-21 12:21:59 +0200

    protected routes

--- controllers/channelController.js
+++ controllers/channelController.js
@@ -4,7 +4,7 @@ const Channel = require('../models/channel');
 const get = async (req, res) => {
     const username = req.user ? req.user.username : "";
     const channels = await Channel.find({ owner: username });
-    console.log(channels);
+
     res.render('channels', {
         channels,
         username: req.user ? req.user.username : '',
