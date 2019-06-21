bd910b52a1fca0842aab78a9dfb7e3719fe9b003
Nathan Keyzer <nathankeyzer@043.147.028.145.hva.nl>
2019-06-21 17:07:53 +0200

    double quotes

--- routes/channel.js
+++ routes/channel.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const channelController = require('../controllers/channelController');
+const express = require("express")
+const channelController = require("../controllers/channelController");
 
 module.exports.autoroute = {
     get: {
-        '/kanalen': channelController.get,
-        '/kanalen/toevoegen': channelController.add,
-        '/kanalen/:id': channelController.update
+        "/kanalen": channelController.get,
+        "/kanalen/toevoegen": channelController.add,
+        "/kanalen/:id": channelController.update
     },
 
     post: {
-        '/kanalen': [express.json(), channelController.create]
+        "/kanalen": [express.json(), channelController.create]
     },
 
     patch: {
-        '/kanalen/:id': [express.json(), channelController.save]
+        "/kanalen/:id": [express.json(), channelController.save]
     }
 };
