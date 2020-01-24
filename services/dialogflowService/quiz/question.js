b3fa9cb286093a1d996d30d0dbfb7549017f4ed0
Arash Paknezad <a.paknezad@hotmail.com>
2020-01-24 21:27:55 +0100

    undo fix

--- services/dialogflowService/quiz/question.js
+++ services/dialogflowService/quiz/question.js
@@ -3,13 +3,12 @@ const List = require('../../../models/list');
 // question intent
 const question = async agent => {
     const listIdContext = agent.context.get('intent_oefenen');
-    const vraagContext = agent.context.get("vraag-context");
-    const correctAnswers = vraagContext ? vraagContext.parameters.correctAnswers : 0;
+    const vraagContext = agent.context.get("vraag-context") ? agent.context.get("vraag-context") : undefined;
+    const correctAnswers = vraagContext.parameters ? vraagContext.parameters.correctAnswers : 0;
 
     let listId = listIdContext ? listIdContext.parameters.listId : vraagContext.parameters.listId;
-    let vragen;
 
-    if (!vraagContext || !vraagContext.parameters || !vraagContext.parameters.vragen) {
+    if (!vraagContext.parameters || !vraagContext.parameters.vragen) {
         agent.add(`Oke, gaan we ${listIdContext.parameters.listName} doen.`);
 
         const exercise = await List.findOne({_id: listId});     // get questions from database by listId
